05c136511d2c209589ce844be3591b09948742ec
MIJanda <mokiliisaacjanda@gmail.com>
2024-07-16 01:21:24 +0300

    feat: add a profile tab
    - create a profile tab for the user

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -73,9 +73,21 @@ export default function TabsLayout() {
 						)
 					}}
 				/>
-
-
-
+				<Tabs.Screen
+					name='profile'
+					options={{
+						title: 'Profile',
+						headerShown: false,
+						tabBarIcon: ({ color, focused }) => (
+							<TabIcon
+								icon={icons.profile}
+								color={color}
+								name="Profile"
+								focused={focused}
+							/>
+						)
+					}}
+				/>
 			</Tabs>
 		</>
 	);
