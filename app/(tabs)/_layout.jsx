786de03950f1996b3efe7e63d6955d613ea30290
MIJanda <mokiliisaacjanda@gmail.com>
2024-07-16 01:35:25 +0300

    feat: complete tab bar styles
    - completed the tab bar styling

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -13,6 +13,7 @@ const TabIcon = ({ icon, color, name, focused }) => {
 			/>
 			<Text
 				className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`}
+				style={{ color: color }}
 			>
 				{name}
 			</Text>
