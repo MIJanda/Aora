f76faaed43a4501c6b2326a44cb8de0c4e344d61
MIJanda <mokiliisaacjanda@gmail.com>
2024-07-16 08:41:01 +0300

    feat: implement view
    - implemented the apps scrollable view

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
 	return (
-		<View className="flex-1 items-center justify-center bg-white">
-			<Text className="text-3xl font-pblack">Aora!</Text>
-			<StatusBar style="auto" />
-			<Link href="/home" style={{ color: 'blue' }}>
-				Go Home
-			</Link>
-		</View>
+		<SafeAreaView className="bg-primary h-full">
+			<ScrollView contentContainerStyle={{ height: '100%' }}>
+				<View className="w-full justify-center items-center h-full px-4">
+
+				</View>
+			</ScrollView>
+		</SafeAreaView>
 	);
 }
 
