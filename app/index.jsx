5de29de38c69f86c767a6959045c10f0b6eff38a
MIJanda <mokiliisaacjanda@gmail.com>
2024-07-16 10:14:56 +0300

    feat: add onboarding text

--- app/index.jsx
+++ app/index.jsx
@@ -19,6 +19,23 @@ export default function App() {
 						className="max-w-[380px] w-full h-[300px]"
 						resizeMode="contain"
 					/>
+					<View className="relative mt-5">
+						<Text className="text-3xl text-white font-bold text-center">
+							Discover Endless Possibilities with {' '}
+							<Text className="text-secondary-200">
+								Aora
+							</Text>
+						</Text>
+						<Image
+							className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+							source={images.path}
+							resizeMode="contain"
+						/>
+					</View>
+					<Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+						Where Creativity Meets Innovation: Embark on a Journey of Limitless Exploration with Aora
+					</Text>
+
 				</View>
 			</ScrollView>
 		</SafeAreaView>
